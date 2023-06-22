59b4ea5e85f8ba123f86c7fb74730e2df765fbb3
Grubenko Maxim <mdgrubenko@maieducation>
2023-06-22 23:19:44 +0300

    edit map component

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,7 +3,7 @@ import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 const MapContainer = () => {
   return (
-      <YMaps  query={{ apikey: '8d5cb0e3-0fdb-4747-b43b-041d2a0c7f6b\n' }}>
+      <YMaps  query={{ apikey: '8d5cb0e3-0fdb-4747-b43b-041d2a0c7f6b' }}>
         <div style={{ width: '100%', height: 'auto' }}>
         <Map
             defaultState={{ center: [55.810237, 37.498872], zoom: 14.5 }}
