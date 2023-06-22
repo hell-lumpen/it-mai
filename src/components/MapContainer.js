c3d56fd5b731cc1d0c3c44d1aa337efa44f6e10c
Grubenko Maxim <mdgrubenko@maieducation>
2023-06-22 23:49:35 +0300

    edit map component styles

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import '../App.css'
 
 const MapContainer = () => {
   return (
       <YMaps query={{ apikey: '8d5cb0e3-0fdb-4747-b43b-041d2a0c7f6b' }}>
-        <div style={{ width: '100%', height: '100vh' }}>
+        <div className={'map-block'}>
           <Map
               defaultState={{ center: [55.810237, 37.498872], zoom: 14.5 }}
           >
