2864d095e4af1e61963c336b149f00a972f3a185
Grubenko Maxim <mdgrubenko@maieducation>
2023-06-22 23:46:23 +0300

    edit map component styles

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -3,14 +3,14 @@ import { YMaps, Map, Placemark } from 'react-yandex-maps';
 
 const MapContainer = () => {
   return (
-      <YMaps  query={{ apikey: '8d5cb0e3-0fdb-4747-b43b-041d2a0c7f6b' }}>
-        <div style={{ width: '100%', height: 'auto' }}>
-        <Map
-            defaultState={{ center: [55.810237, 37.498872], zoom: 14.5 }}
-            width="100%" height="100%"
-        >
-          <Placemark geometry={[55.810237, 37.498872]} />
-        </Map>
+      <YMaps query={{ apikey: '8d5cb0e3-0fdb-4747-b43b-041d2a0c7f6b' }}>
+        <div style={{ width: '100%', height: '100vh' }}>
+          <Map
+              defaultState={{ center: [55.810237, 37.498872], zoom: 14.5 }}
+              style={{ width: '100%', height: '100%' }}
+          >
+            <Placemark geometry={[55.810237, 37.498872]} />
+          </Map>
         </div>
       </YMaps>
   );
