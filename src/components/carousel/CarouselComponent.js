ee365ee29813d1a7423fad59fd54017a1c793a81
Grubenko Maxim <mdgrubenko@maieducation>
2023-06-25 20:36:35 +0300

    add carousel styles

--- src/components/carousel/CarouselComponent.js
+++ src/components/carousel/CarouselComponent.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import { Carousel } from 'react-responsive-carousel';
-import '../../App.css'
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import './CarouselComponent.css'; // Импортируйте файл со стилями
+import './CarouselComponent.css';
 
 const CarouselComponent = ({ items, settings }) => {
   return (
-      <Carousel {...settings}>
-        {items.map((item, index) => (
-            <div key={index} className="carousel-item">
-              <div className="carousel-image">
-                {item.image && <img src={item.image} alt={item.alt} />}
+      <div className="carousel">
+        <Carousel {...settings}>
+          {items.map((item, index) => (
+              <div key={index} className="carousel-item">
+                {item.image && (
+                    <div className="carousel-image">
+                      <img src={item.image} alt={item.image} />
+                    </div>
+                )}
+                <div className="carousel-content">
+                  {item.title && <h3 className="section-title">{item.title}</h3>}
+                  {item.description && <p className="section-text">{item.description}</p>}
+                  {item.link && (
+                      <a
+                          href={item.link}
+                          style={{ textDecoration: 'none', color: '#007ACD' }}
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                        Подробнее
+                      </a>
+                  )}
+                </div>
               </div>
-              <div className="carousel-content">
-                {item.title && <h3 className='section-title'>{item.title}</h3>}
-                {item.description && <p className='section-text'>{item.description}</p>}
-                {item.link && <a href={item.link} style={{textDecoration: 'none', color: '#007ACD'}} target="_blank" rel="noopener noreferrer">Подробнее</a>}
-              </div>
-            </div>
-        ))}
-      </Carousel>
+          ))}
+        </Carousel>
+      </div>
   );
 };
 
