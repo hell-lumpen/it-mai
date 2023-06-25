f7ff2ce00852c68b0b1461362b13fdf6591ce246
Grubenko Maxim <mdgrubenko@maieducation>
2023-06-25 13:45:31 +0300

    add content

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -22,7 +22,7 @@ const Header = () => {
               <Link to="/bachelors/fi">Фундаментальная информатика и информационные технологии</Link>
             </li>
             <li className={`menu-item ${location.pathname === '/admission/application' ? 'active' : ''}`}>
-              <Link to="/admission/application">Подать заявку</Link>
+              <Link to="/admission/application">Поступление 2023</Link>
             </li>
           </ul>
         </nav>
