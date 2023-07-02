7f2a9bb2f5196dbf87467f4cc60316ef2a2df1d3
Grubenko Maxim <mdgrubenko@maieducation>
2023-07-02 23:52:24 +0300

    edit styles

--- src/components/faqsection/FAQSection.js
+++ src/components/faqsection/FAQSection.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 import './FAQSection.css';
+import {useInView} from "react-intersection-observer";
 
 const FAQSection = ({ faqs }) => {
   const [selectedFaq, setSelectedFaq] = useState(null);
@@ -13,37 +14,43 @@ const FAQSection = ({ faqs }) => {
     }
   };
 
-  const faqVariants = {
-    hidden: { opacity: 0, y: -10 },
-    visible: { opacity: 1, y: 0 },
-  };
-
   return (
       <div className="faq-section">
-        {faqs.map((faq, index) => (
-            <motion.div
-                className="faq-item"
-                key={index}
-                initial="hidden"
-                animate="visible"
-                variants={faqVariants}
-                transition={{ duration: 0.3, delay: index * 0.1 }}
-            >
-              <div
-                  className={`faq-question ${selectedFaq === index ? 'active' : ''}`}
-                  onClick={() => handleFaqClick(index)}
+        {faqs.map((faq, index) => {
+          // eslint-disable-next-line react-hooks/rules-of-hooks
+          const [ref, inView] = useInView({
+            triggerOnce: true,
+            rootMargin: '-20px 0px', // Регулируйте этот отступ по своему усмотрению
+          });
+
+          const faqVariants = {
+            hidden: { opacity: 0, y: -10 },
+            visible: { opacity: 1, y: 0 },
+          };
+
+          return (
+              <motion.div
+                  className="faq-item"
+                  key={index}
+                  ref={ref}
+                  initial="hidden"
+                  animate={inView ? 'visible' : 'hidden'}
+                  variants={faqVariants}
+                  transition={{ duration: 0.3, delay: index * 0.1 }}
               >
-                {faq.question}
-                <span className="faq-arrow">{selectedFaq === index ? '▲' : '▼'}</span>
-              </div>
-              {selectedFaq === index && (
-                  <p
-                      className="faq-answer"
-                      dangerouslySetInnerHTML={{ __html: faq.answer }}
-                  />
-              )}
-            </motion.div>
-        ))}
+                <div
+                    className={`faq-question ${selectedFaq === index ? 'active' : ''}`}
+                    onClick={() => handleFaqClick(index)}
+                >
+                  {faq.question}
+                  <span className="faq-arrow">{selectedFaq === index ? '▲' : '▼'}</span>
+                </div>
+                {selectedFaq === index && (
+                    <p className="faq-answer" dangerouslySetInnerHTML={{ __html: faq.answer }} />
+                )}
+              </motion.div>
+          );
+        })}
       </div>
   );
 };
